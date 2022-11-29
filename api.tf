f3633072f3088e9dbce77567a161105321ee8e6a
lvprasadv <84435748+lvprasadv@users.noreply.github.com>
2022-11-29 14:19:14 +0530

    Update api.tf

--- api.tf
+++ api.tf
@@ -18,3 +18,24 @@ resource "google_project_service" "osconfig_api" {
    service   = "osconfig.googleapis.com"
    disable_dependent_services = true
 }
+
+######### enable metadata config ############
+resource "google_compute_project_metadata_item" "osconfig_enable_meta" {
+   
+   count = length(data.google_project.project[*].project_id)
+   project = data.google_project.project[count.index].project_id
+
+  key        = "enable-osconfig"
+  value      = "TRUE"
+  depends_on = [google_project_service.iam_api]
+}
+
+resource "google_compute_project_metadata_item" "osconfig_log_level_meta" {
+
+   count = length(data.google_project.project[*].project_id)
+   project = data.google_project.project[count.index].project_id
+
+  key        = "osconfig-log-level"
+  value      = "debug"
+  depends_on = [google_project_service.iam_api]
+}

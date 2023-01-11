7b402ff47b7bf54713f912ef1d49d00cbd7c4886
lvprasadv <84435748+lvprasadv@users.noreply.github.com>
2023-01-11 16:03:59 +0530

    Update varaiable.tf

--- varaiable.tf
+++ varaiable.tf
@@ -1,5 +1,27 @@
 variable "proj_id" {
   description = "Pass specific proj id for enabling api"
   type        = list(string)
-  #default     = ["", "", ""]
+  default     = ["", "", ""]
 }
+
+ variable "bucket_name" {
+   description = "Bucket where exe are placed"
+   type        = string
+}
+
+ variable "bucket_name" {
+   description = "Bucket where exe are placed"
+   type        = string
+}
+
+ variable "gen_number_linux_presnapshot" {
+   description = "Generation number of linux presnapshot available in GCS"
+   type        = string
+}
+
+ variable "gen_number_windows_presnapshot" {
+   description = "Generation number of windows presnapshot available in GCS"
+   type        = string
+}
+
+
